6eb107ae9a2528443636b6cd9f9e33f78e9cb1d3
Shubham-front384 <shubhamworkemail07@gmail.com>
2025-12-23 09:06:49 +0530

    Day 3 with GSAP Timeline done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,39 @@
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+
 import "./Card.css";
 
 const Card = ({ user }) => {
   const { name, tagline, img, logoImg, backgroundColor } = user;
+  const cardRef = useRef();
+  const logoRef = useRef();
+  const tl = useRef();
+
+  useLayoutEffect(() => {
+    tl.current = gsap.timeline({ paused: true });
+
+    tl.current
+      .to(cardRef.current, {
+        y: -10,
+        scale: 1.03,
+        duration: 0.3,
+        ease: "power3.out",
+      })
+      .to(
+        logoRef.current,
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 0.3,
+        },
+        "<"
+      );
+    
+    return () => tl.current.kill();
+  }, []);
 
   return (
-    <div className="card-info" style={{ backgroundColor }}>
+    <div className="card-info" style={{ backgroundColor }} ref={cardRef} onMouseEnter={() => tl.current.play()} onMouseLeave={() => tl.current.reverse()}>
       <h4 className="card-heading">
         <span>{name}</span>
         <span>{tagline}</span>
@@ -13,7 +42,7 @@ const Card = ({ user }) => {
       <img src={img} alt={name} className="card-img" />
 
       <div className="card-details">
-        <img src={logoImg} alt={`${name} logo`} />
+        <img src={logoImg} alt={`${name} logo`} ref={logoRef} />
         <h5 className="logo-name">
           <span>angga</span>
           <span>tantama</span>
